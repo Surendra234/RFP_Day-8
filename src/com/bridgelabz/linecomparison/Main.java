14a8968d7ed95066c1153de5ff8cb70eb4b8ecf1
Surendra234 <surendramahawar873@gmail.com>
2022-05-24 17:34:20 +0530

    Calculate the length of lines

--- src/com/bridgelabz/linecomparison/Main.java
+++ src/com/bridgelabz/linecomparison/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison program");
+
+        LineComparison line1 = new LineComparison(23, 25, 27, 29);
+        double lengthOfLine1 = line1.lengthOfLine();
+        System.out.println("Length of line one = " + lengthOfLine1);
+
+        LineComparison line2 = new LineComparison(32, 36, 38, 39);
+        double lengthOfLine2 = line2.lengthOfLine();
+        System.out.println("Length of line two = " + lengthOfLine2);
     }
 }
